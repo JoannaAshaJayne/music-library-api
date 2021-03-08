2fd9488478c8b1b48046151a29f5d9b42474d295
Joanna Hughes <joannahughes@hotmail.co.uk>
2021-03-08 21:05:01 +0000

    commit to main

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { Artist } = require('./models');
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/artists", (req, res) => {
+    Artist.create(req.body).then(artist => res.status(201).json(artist));
+});
 
 module.exports = app
