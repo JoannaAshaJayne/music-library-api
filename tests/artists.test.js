04f7a898ba80899ce99a9767791ccd9ddf83ab62
Joanna Hughes <joannahughes@hotmail.co.uk>
2021-03-08 17:09:10 +0000

    commit to main

---
+++ tests/artists.test.js
@@ -0,0 +1,62 @@
+/* tests/artists.test.js */
+const { expect } = require('chai');
+const request = require('supertest');
+const { Artist } = require('../src/models');
+const app = require('../src/app');
+
+describe('/artists', () => {
+  before(done => {
+    Artist.sequelize
+      .sync()
+      .then(() => done())
+      .catch(error => done(error));
+  })
+
+  beforeEach(done => {
+    Artist.destroy({ where: {} })
+      .then(() => done()).catch(error => done(error));
+  })
+
+  describe('POST /artists', (done) => {
+    it('creates a new artist in the database', () => {
+      request(app).post('/artists').send({
+        name: 'Tame Impala',
+        genre: 'Rock',
+      }).then(response => {
+        expect(response.status).to.equal(201);
+        done();
+      }).catch(error => done(error));
+    });
+  });
+  /* tests/artists.test.js */
+  describe('with artists in the database', () => {
+    let artists;
+    beforeEach((done) => {
+      Promise.all([
+        Artist.create({ name: 'Tame Impala', genre: 'Rock' }),
+        Artist.create({ name: 'Kylie Minogue', genre: 'Pop' }),
+        Artist.create({ name: 'Dave Brubeck', genre: 'Jazz' }),
+      ]).then((documents) => {
+        artists = documents;
+        done();
+      });
+    });
+
+    describe('GET /artists', () => {
+      it('gets all artist records', (done) => {
+        request(app)
+          .get('/artists')
+          .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body.length).to.equal(3);
+            res.body.forEach((artist) => {
+              const expected = artists.find((a) => a.id === artist.id);
+              expect(artist.name).to.equal(expected.name);
+              expect(artist.genre).to.equal(expected.genre);
+            });
+            done();
+          })
+          .catch(error => done(error));
+      });
+    });
+  });
---
+++ tests/songs.test.js
@@ -0,0 +1,15 @@
+it('creates a new song under an album', (done) => {
+    request(app)
+      .post(`/album/${album.id}/song`)
+      .send({
+        artist: artist.id,
+        name: 'Solitude Is Bliss',
+      })
+      .then((res) => {
+        expect(res.status).to.equal(201);
+        expect(res.body.name).to.equal('Solitude Is Bliss');
+        expect(res.body.artistId).to.equal(artist.id);
+        expect(res.body.albumId).to.equal(album.id);
+        done();
+      }).catch(error => done(error));
+  });
